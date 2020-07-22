61a1bc95c4fdc8d36c2a23bab18d1c433152946f
FlyInWind <2518509078@qq.com>
2020-07-22 15:03:46 +0800

    init commit

---
+++ src/main/kotlin/Ext.kt
@@ -0,0 +1,21 @@
+object V {
+    const val commonsCollections4 = "4.4"
+    const val commonsLang3 = "3.10"
+
+    const val flowable = "6.5.0"
+
+    const val p6spy = "3.9.0"
+
+    const val reactor = "3.3.7.RELEASE"
+
+    const val springDoc = "1.4.1"
+    const val springManagement = "1.0.9.RELEASE"
+    const val springBoot = "2.3.1.RELEASE"
+
+    const val mapstruct = "1.4.0.Beta1"
+    const val mariaJdbc = "2.6.1"
+    const val mybatisPlus = "3.3.2"
+
+    const val kotlin = "1.3.72"
+    const val kotlinLogging = "1.7.9"
+}
