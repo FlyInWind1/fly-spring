bbcf9595d59871e02e1ca11a3f82c0a4188757b2
FlyInWind <2518509078@qq.com>
2020-08-01 23:01:38 +0800

    update

--- src/main/kotlin/V.kt
+++ src/main/kotlin/V.kt
@@ -4,17 +4,21 @@ object V {
 
     const val flowable = "6.5.0"
 
+    const val jackson = "2.11.0"
+
     const val lombok = "1.18.12"
 
     const val p6spy = "3.9.0"
 
     const val reactor = "3.3.7.RELEASE"
 
+    const val spring = "5.2.7.RELEASE"
     const val springDoc = "1.4.1"
     const val springManagement = "1.0.9.RELEASE"
-    const val springBoot = "2.3.1.RELEASE"
+    const val springBoot = "2.3.2.RELEASE"
 
     const val mapstruct = "1.4.0.Beta1"
+    const val mapstructKotlin = "1.3.1.2"
     const val mariaJdbc = "2.6.1"
     const val mybatisPlus = "3.3.2"
 
