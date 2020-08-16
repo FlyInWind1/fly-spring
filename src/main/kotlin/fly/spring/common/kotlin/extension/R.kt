5f70a819d6c155e64a15f6a9cd9a08213b781806
FlyInWind <2518509078@qq.com>
2020-08-17 00:37:29 +0800

    rename fun

--- src/main/kotlin/fly/spring/common/kotlin/extension/R.kt
+++ src/main/kotlin/fly/spring/common/kotlin/extension/R.kt
@@ -3,6 +3,6 @@ package fly.spring.common.kotlin.extension
 import fly.spring.common.pojo.R
 import reactor.core.publisher.Mono
 
-fun <T> T.Rok(): Mono<R<T>> = R.ok(this)
+fun <T> T.rok(): Mono<R<T>> = R.ok(this)
 
-fun <T> T.Rok(msg: String): Mono<R<T>> = R.ok(this, msg)
+fun <T> T.rok(msg: String): Mono<R<T>> = R.ok(this, msg)
