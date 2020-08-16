12a3e5fbf15258918ed5b53da0c877ee71042a8d
FlyInWind <2518509078@qq.com>
2020-08-17 00:41:19 +0800

    some fix

--- src/main/kotlin/fly/spring/common/exception/ExceptionHandler.kt
+++ src/main/kotlin/fly/spring/common/exception/ExceptionHandler.kt
@@ -1,20 +1,21 @@
 package fly.spring.common.exception
 
 import com.fasterxml.jackson.core.JsonParseException
-import fly.spring.common.core.pojo.R
+import fly.spring.common.pojo.R
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import reactor.core.publisher.Mono
 
 @RestControllerAdvice
 class ExceptionHandler {
 
     @ExceptionHandler(JsonParseException::class, org.springframework.boot.json.JsonParseException::class)
-    fun jsonParseException(exception: Exception): R<String> {
+    fun jsonParseException(exception: Exception): Mono<R<String?>> {
         return R.failed(exception.message)
     }
 
     @ExceptionHandler(Exception::class)
-    fun exception(exception: Exception): R<String> {
+    fun exception(exception: Exception): Mono<R<String?>> {
         return R.failed(exception.message)
     }
 }
