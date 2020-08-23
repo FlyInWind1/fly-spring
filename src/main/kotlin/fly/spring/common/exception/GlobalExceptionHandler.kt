a09da1442ca4d0f0142475439361b9992669235f
FlyInWind <2518509078@qq.com>
2020-08-23 15:16:47 +0800

    print more info for GlobalExceptionHandler

--- src/main/kotlin/fly/spring/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/fly/spring/common/exception/GlobalExceptionHandler.kt
@@ -3,25 +3,29 @@ package fly.spring.common.exception
 import com.fasterxml.jackson.core.JsonParseException
 import fly.spring.common.pojo.R
 import mu.KotlinLogging
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import reactor.core.publisher.Mono
 
 @RestControllerAdvice
-class ExceptionHandler {
+class GlobalExceptionHandler {
     companion object {
         val log = KotlinLogging.logger { }
     }
 
     @ExceptionHandler(JsonParseException::class, org.springframework.boot.json.JsonParseException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun jsonParseException(exception: Exception): Mono<R<String?>> {
-        log.warn(exception.message, exception)
+        log.warn("JsonParse exception:\n {}", exception.message, exception)
         return R.failed(exception.message)
     }
 
     @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun exception(exception: Exception): Mono<R<String?>> {
-        log.warn(exception.message, exception)
+        log.warn("Global exception:\n {}", exception.message, exception)
         return R.failed(exception.message)
     }
 }
