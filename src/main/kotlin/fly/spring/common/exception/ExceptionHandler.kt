10774cbd6b1cdaf80bdc15bd0adb166269969b3b
FlyInWind <2518509078@qq.com>
2020-08-23 14:20:40 +0800

    print exception stack trace

--- src/main/kotlin/fly/spring/common/exception/ExceptionHandler.kt
+++ src/main/kotlin/fly/spring/common/exception/ExceptionHandler.kt
@@ -2,20 +2,26 @@ package fly.spring.common.exception
 
 import com.fasterxml.jackson.core.JsonParseException
 import fly.spring.common.pojo.R
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import reactor.core.publisher.Mono
 
 @RestControllerAdvice
 class ExceptionHandler {
+    companion object {
+        val log = KotlinLogging.logger { }
+    }
 
     @ExceptionHandler(JsonParseException::class, org.springframework.boot.json.JsonParseException::class)
     fun jsonParseException(exception: Exception): Mono<R<String?>> {
+        log.warn(exception.message, exception)
         return R.failed(exception.message)
     }
 
     @ExceptionHandler(Exception::class)
     fun exception(exception: Exception): Mono<R<String?>> {
+        log.warn(exception.message, exception)
         return R.failed(exception.message)
     }
 }
